d207244ae16892ec4de368ed2de242b33056e7ce
Fabio Mano <Mano@Administrators-MacBook-Pro-2.local>
2020-10-03 21:12:44 +0100

    some tests

--- src/app/goals.service.spec.ts
+++ src/app/goals.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GoalsService } from './goals.service';
+import { Goal } from './models'
+import { GOALS } from './mock-goals';
 
 describe('GoalsService', () => {
   let service: GoalsService;
@@ -13,4 +15,13 @@ describe('GoalsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('getGoals', () => {
+    it('should return list of goals', async () => {
+      const output = service.getGoals();
+      output.subscribe(result => {
+        expect(result).toEqual(GOALS);
+      })
+    });
+  })
 });
